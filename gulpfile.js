e18436bfd8da61246f74295114ad883112790ce1
Mikael Härsjö <mikael.harsjo@cryptzone.com>
2014-10-10 14:41:53 +0200

    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
+var karma = require('karma').server;
+
+gulp.task('karma', function() {
+  karma.start({configFile : __dirname +'/karma.conf.js', singleRun: true});
+});
 
 gulp.task('default', function () {
 
